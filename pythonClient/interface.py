fa89642535f8e206446ceab227a050ce16681eb8
rzymski <83671265+rzymski@users.noreply.github.com>
2024-04-29 13:28:08 +0200

    Coś źle się rozciągają linie wierszy

--- pythonClient/interface.py
+++ pythonClient/interface.py
@@ -13,25 +13,20 @@ class AirportInterface:
         self.root.geometry(f"{self.screen_width}x{self.screen_height}+0+0")
         root.state("zoomed")
         # Top frame, its weights and buttons login, register, logout
-        self.topframe = LabelFrame(self.root, padx=10, pady=10, labelanchor="n")
-        self.topframe.pack(side="top", fill="both")
-        self.topframe.grid_columnconfigure(0, weight=1)  # Kolumna do rozciągania
-        self.topframe.grid_columnconfigure(1, weight=0)  # Kolumna dla przycisku login
-        self.topframe.grid_columnconfigure(2, weight=0)  # Kolumna dla przycisku register
-        # self.loggedUserLabel = Label(self.topframe, text="Zalogowany uzytkownik")
-        # self.loggedUserLabel.grid(row=0, column=0)
-        # self.loggedUserLabel['font'] = boldFont18
-        self.loggedUserLabel = self.createLabel(self.topframe, "NAZWA UZYTKOWNIKA", grid=[0, 0], labelFont=[32, "bold"])
-        self.loginButton = self.createButton(self.topframe, text="Zaloguj się", command=self.login, pad=[20, 20], grid=[0, 1], buttonFont=[24, "bold"])
-        self.registerButton = self.createButton(self.topframe, text="Zarejestruj się", command=self.register, pad=[20, 20], grid=[0, 2], buttonFont=[24, "bold"])
-        self.logoutButton = self.createButton(self.topframe, text="Wyloguj się", command=self.logout, pad=[20, 20], grid=[0, 3], buttonFont=[24, "bold"])
+        self.topFrame = self.createLabelFrame(self.root, pad=[10, 10], side="top", fill="both")
+        self.topFrame.grid_columnconfigure(0, weight=1)  # Kolumna do rozciągania
+        self.topFrame.grid_columnconfigure(1, weight=0)  # Kolumna dla przycisku login
+        self.topFrame.grid_columnconfigure(2, weight=0)  # Kolumna dla przycisku register
+        # self.loggedUserLabel = Label(self.topFrame, text="Zalogowany uzytkownik")
+        self.loggedUserLabel = self.createLabel(self.topFrame, text="NAZWA UZYTKOWNIKA", grid=[0, 0], labelFont=[32, "bold"])
+        self.loginButton = self.createButton(self.topFrame, text="Zaloguj się", command=self.login, pad=[20, 20], grid=[0, 1], buttonFont=[24, "bold"])
+        self.registerButton = self.createButton(self.topFrame, text="Zarejestruj się", command=self.register, pad=[20, 20], grid=[0, 2], buttonFont=[24, "bold"])
+        self.logoutButton = self.createButton(self.topFrame, text="Wyloguj się", command=self.logout, pad=[20, 20], grid=[0, 3], buttonFont=[24, "bold"])
         # Left frame buttons checkFlights, findFlight, reserve flight, cancel reservation
-        self.leftFrame = LabelFrame(self.root, padx=0, pady=0, labelanchor="w")
-        self.leftFrame.pack(side="left", fill="both")
-        self.checkFlightsButton = self.createButton(self.leftFrame, text="Sprawdź listę lotów", command=self.checkFlightList, pad=[5, 5], grid=[0, 0], buttonFont=[28, "bold"])
-        self.findFlightLabel = Label(self.leftFrame)
-        self.findFlightLabel.grid(row=1, column=0, sticky="WE")
-        self.findFlightButton = self.createButton(self.findFlightLabel, text="Wyszukaj lot", command=self.findFlight, pad=[20, 20], grid=[0, 0], span=(2, 1), buttonFont=[18, "bold"])
+        self.leftFrame = self.createLabelFrame(self.root, pad=[0, 0], side="left", fill="both")
+        self.checkFlightsButton = self.createButton(self.leftFrame, text="Sprawdź listę lotów", command=self.checkFlightList, pad=[10, 10], grid=[0, 0], buttonFont=[28, "bold"])
+        self.findFlightLabel = self.createLabel(self.leftFrame, text='', grid=[1, 0], sticky="WE")
+        self.findFlightButton = self.createButton(self.findFlightLabel, text="Wyszukaj lot", command=self.findFlight, pad=[10, 10], grid=[0, 0], span=(2, 1), buttonFont=[28, "bold"])
         # dropdown departure city
         self.fromLabel = self.createLabel(self.findFlightLabel, "Z:", grid=[0, 1], labelFont=[18, "bold"])
         self.fromAirportVar = StringVar()
@@ -49,16 +44,75 @@ class AirportInterface:
         self.toDropdown['font'] = boldFont18
         self.toDropdown.grid(row=1, column=2)
         # reserve button
-        self.reserveFlightButton = self.createButton(self.leftFrame, text="Zarezerwuj lot", command=self.reserveFlight, pad=[20, 20], grid=[2, 0], buttonFont=[18, "bold"])
+        self.reserveFlightButton = self.createButton(self.leftFrame, text="Zarezerwuj lot", command=self.reserveFlight, pad=[10, 10], grid=[2, 0], buttonFont=[28, "bold"])
         # cancel reservation
-        self.cancelReservationButton = self.createButton(self.leftFrame, text="Anuluj rezerwacje", command=self.cancelReservation, pad=[20, 20], grid=[3, 0], buttonFont=[18, "bold"])
+        self.cancelReservationButton = self.createButton(self.leftFrame, text="Anuluj rezerwacje", command=self.cancelReservation, pad=[10, 10], grid=[3, 0], buttonFont=[28, "bold"])
+        # flights
+        self.flightsLabel = self.createLabelFrame(self.root, expand=True, pad=[0, 0])
+
+        self.flightsHeader = self.createLabel(self.flightsLabel, pack=[None, False, "x"], pad=[10, 10])
+        self.flightsHeaderFlightCode = self.createLabel(self.flightsHeader, "KOD LOTU", pack=["left", True, "x"], pad=[20, 10])
+        self.flightsHeaderDepartureAirport = self.createLabel(self.flightsHeader, "LOTNISKO ODLOTU", pack=["left", True, "x"], pad=[20, 10])
+        self.flightsHeaderDepartureTime = self.createLabel(self.flightsHeader, "CZAS ODLOTU", pack=["left", True, "x"], pad=[20, 10])
+        self.flightsHeaderDestinationAirport = self.createLabel(self.flightsHeader, "LOTNISKO DOCELOWE", pack=["left", True, "x"], pad=[20, 10])
+        self.flightsHeaderArrivalTime = self.createLabel(self.flightsHeader, "CZAS PRZYLOTU", pack=["right", True, "x"], pad=[20, 10])
+
+
         # hide buttons which shouldn't be displayed to not logged user
         self.hideButtonsAndLabels([self.loggedUserLabel, self.logoutButton, self.reserveFlightButton, self.cancelReservationButton])  # ukrycie przyciskow rezerwowania i usuwania rezerwacji
         # self.showButtonsAndLabels([[self.reserveFlightButton, (2, 0, "WE")], [self.cancelReservationButton, (3, 0, "WE")]])  # wyswietlenie przyciskow rezerwowania i usuwania rezerwacji
 
-    def createLabel(self, frame, text, grid, sticky="WE", span=(1, 1), labelFont=[18, "bold"]):
-        label = Label(frame, text=text)
-        label.grid(row=grid[0], column=grid[1], rowspan=span[0], columnspan=span[1], sticky=sticky)
+        self.scrollableFrame = self.createScrollableFrame(self.flightsLabel)
+
+
+        for i in range(200):
+            # label = Label(self.scrollableFrame, text=f"Etykieta {i + 1}", padx=10, pady=5, font=font.Font(size=12))
+            # label.pack(anchor="w")
+            label = self.createFlightRow(self.scrollableFrame, f"Flight {i + 1}")
+
+    def createFlightRow(self, frame, flightCode="KOD", departureAirport="ODLOT Z", departureTime="YYYY-MM-DD HH:mm:ss", destinationAirport="PRZYLOT DO", arrivalTime="YYYY-MM-DD HH:mm:ss"):
+        mainLabel = self.createLabel(frame, pack=[None, True, "x"], pad=[5, 5])
+        flightsHeaderFlightCode = self.createLabel(mainLabel, flightCode, pack=["left", True, "x"], pad=[20, 5])
+        flightDepartureAirport = self.createLabel(mainLabel, departureAirport, pack=["left", True, "x"], pad=[20, 5])
+        flightDepartureTime = self.createLabel(mainLabel, departureTime, pack=["left", True, "x"], pad=[20, 5])
+        flightDestinationAirport = self.createLabel(mainLabel, destinationAirport, pack=["left", True, "x"], pad=[20, 5])
+        flightArrivalTime = self.createLabel(mainLabel, arrivalTime, pack=["right", True, "x"], pad=[20, 5])
+        return mainLabel
+
+    def createScrollableFrame(self, parent, padx=10, pady=10):
+        # Utworzenie ramki zawierającej Canvas i Scrollbar
+        container = Frame(parent)
+        container.pack(expand=True, fill="both")
+        # Utworzenie Canvas i pionowego paska przewijania
+        canvas = Canvas(container, bg="white")
+        scrollbar = Scrollbar(container, orient="vertical", command=canvas.yview)
+        scrollbar.pack(side="right", fill="y")
+        canvas.pack(side="left", expand=True, fill="both")
+        # Konfiguracja Canvas i tworzenie okna wewnątrz Canvas
+        innerFrame = Frame(canvas)
+        canvas.create_window((0, 0), window=innerFrame, anchor="nw")
+        # Konfiguracja Scrollbar
+        canvas.configure(yscrollcommand=scrollbar.set)
+
+        # Aktualizacja rozmiaru przewijania
+        def update_scrollregion(event):
+            canvas.config(scrollregion=canvas.bbox("all"))
+        # Ustawienie eventu na zmianę rozmiaru
+        innerFrame.bind("<Configure>", update_scrollregion)
+        return innerFrame
+
+    def createLabelFrame(self, frame, pad, side=None, fill="both", expand=False, text="", labelFont=[18, "bold"]):
+        labelFrame = LabelFrame(frame, padx=pad[0], pady=pad[1], text=text)
+        labelFrame.pack(side=side, fill=fill, expand=expand)
+        labelFrame['font'] = font.Font(size=labelFont[0], weight=labelFont[1])
+        return labelFrame
+
+    def createLabel(self, frame, text='', grid=None, pack=None, sticky="WE", span=(1, 1), pad=[0, 0], labelFont=[18, "bold"]):
+        label = Label(frame, text=text, padx=pad[0], pady=pad[1])
+        if grid:
+            label.grid(row=grid[0], column=grid[1], rowspan=span[0], columnspan=span[1], sticky=sticky)
+        if pack:
+            label.pack(side=pack[0], expand=pack[1], fill=pack[2])
         label['font'] = font.Font(size=labelFont[0], weight=labelFont[1])
         return label
 
